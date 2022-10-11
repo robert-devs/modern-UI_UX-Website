a99e428176d1f60a1e1e0a260a087eb99bfb7e2f
robert-devs <mainrobert04@gmail.com>
2022-10-11 20:43:08 +0300

    initiolizing

---
+++ src/Components/features/feature.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './features.css'
+const features = () => {
+  return (
+    <div>features</div>
+  )
+}
+
+export default features
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './articles/article'
+export {default as Brand} from './Brand/Brand'
+export {default as CTA} from './cta/cta'
+export {default as Features} from './features/feature'
+export {default as Navbar} from './navbar/navbar'
