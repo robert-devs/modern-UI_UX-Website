5f9044d14985c312ef7f00026c82e29d870c98b5
robert-devs <mainrobert04@gmail.com>
2022-10-13 14:21:32 +0300

    adding the Possibilities

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,5 @@
 export {default as Article} from './articles/Article'
 export {default as Brand} from './Brand/Brand'
 export {default as CTA} from './cta/Cta'
-export {default as Features} from './Features/Features'
+export {default as Feature} from './feature/Feature'
 export {default as Navbar} from './navbar/navbar'
